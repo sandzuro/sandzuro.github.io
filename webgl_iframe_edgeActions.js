85fba5234aa2f28ab8ecda9f64ef2f375f174ab3
Alexander Sergievskiy <sandzuro@gmail.com>
2014-07-16 13:07:48 +0400

    1
    1

--- webgl_iframe_edgeActions.js
+++
@@ -1,8 +0,0 @@
-
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
-//Edge symbol: 'stage'
-(function(symbolName){Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){var container=sym.$("Rectangle");container.append('<iframe width="800" height="450" src="https://create.goocreate.com/api/resource/2aa56f3a9bff404280bc47f388b17137.project/latest" title="varg"></iframe>');});
-//Edge binding end
-})("stage");
-//Edge symbol end:'stage'
-})(jQuery,AdobeEdge,"EDGE-642698712");
